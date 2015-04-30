ff934ef8a6a9b44ae067b13217ef4edb649f629e
NicolasDF <ndelfiorentino@gmail.com>
2015-04-30 15:50:42 -0300

    Added GridView and dictionary

--- FuenteInfo/Form1.Designer.cs
+++ FuenteInfo/Form1.Designer.cs
@@ -32,6 +32,9 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.textDir = new System.Windows.Forms.TextBox();
             this.btnExec = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.mDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSearchDir
@@ -42,7 +45,7 @@
             this.btnSearchDir.TabIndex = 0;
             this.btnSearchDir.Text = "Select Directory";
             this.btnSearchDir.UseVisualStyleBackColor = true;
-
+            this.btnSearchDir.Click += new System.EventHandler(this.btnSearchDir_Click_1);
             // 
             // openFileDialog1
             // 
@@ -63,17 +66,41 @@
             this.btnExec.TabIndex = 2;
             this.btnExec.Text = "Execute first order ";
             this.btnExec.UseVisualStyleBackColor = true;
+            this.btnExec.Click += new System.EventHandler(this.btnExec_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // mDataGridView
+            // 
+            this.mDataGridView.AllowUserToAddRows = false;
+            this.mDataGridView.AllowUserToDeleteRows = false;
+            this.mDataGridView.AllowUserToOrderColumns = true;
+            this.mDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mDataGridView.Location = new System.Drawing.Point(57, 162);
+            this.mDataGridView.Name = "mDataGridView";
+            this.mDataGridView.ReadOnly = true;
+            this.mDataGridView.Size = new System.Drawing.Size(507, 150);
+            this.mDataGridView.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(593, 301);
+            this.ClientSize = new System.Drawing.Size(599, 336);
+            this.Controls.Add(this.mDataGridView);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnExec);
             this.Controls.Add(this.textDir);
             this.Controls.Add(this.btnSearchDir);
             this.Name = "Form1";
             this.Text = "Fuentes de Informacion";
+            ((System.ComponentModel.ISupportInitialize)(this.mDataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -85,6 +112,8 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.TextBox textDir;
         private System.Windows.Forms.Button btnExec;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView mDataGridView;
 
     }
 }
--- FuenteInfo/Form1.cs
+++ FuenteInfo/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Windows;
+
 
 namespace FuenteInfo
 {
@@ -16,8 +19,59 @@ namespace FuenteInfo
         {
             InitializeComponent();
         }
+        public class cCharacter
+        {
+            public string Charac { get; set; }
+            public int Cuant { get; set; }
+        }
 
-        private void btnSearchDir_Click(object sender, EventArgs e)
+       
+
+        private void btnExec_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(textDir.Text))
+                {
+                    String line = sr.ReadToEnd();
+                    line = line.ToLower();
+                    label1.Text = line;
+
+                    // crear array de caracteres
+                    char[] charsRead = new char[line.Length];
+                    using (StringReader reader = new StringReader(line))
+                    {
+                       reader.ReadAsync(charsRead, 0, line.Length);
+                    }
+                    // Crear diccionario con todos los char y sus valores
+                    
+                        Dictionary<string, int> dic = new Dictionary<string, int>();
+                        int val;
+                        foreach (char c in charsRead)
+                        {
+                            
+                            if (dic.TryGetValue(c.ToString(),out val))
+                            {
+                                dic[c.ToString()] = val + 1;
+                            }
+                            else
+                            {
+                                dic.Add(c.ToString(), 1);
+                            }
+                        }
+                        mDataGridView.DataSource = dic.ToList();
+                    
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void btnSearchDir_Click_1(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
 
@@ -29,11 +83,9 @@ namespace FuenteInfo
 
             openFileDialog1.ShowDialog();
 
-            // Copy directory name
+            // Copiar el nombre del directorio
 
             textDir.Text = openFileDialog1.FileName.ToString();
-            
- 
         }
 
         
